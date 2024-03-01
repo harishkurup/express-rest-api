bde4ee5d2f897980273e2306a211ac0295d3baf7
Harish Kurup <inbox30@gmail.com>
2024-03-01 13:09:12 +0530

    added express and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello Node Tyscript world!!")
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookeiParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+const app = express();
+
+app.use(cors({
+   credentials: true
+}));
+
+app.use(compression());
+app.use(cookeiParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log("Server running on http://localhost:8080")
+})
